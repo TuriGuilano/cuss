2f93e501ff4c911e16c4ab6a9927af28b9459f2b
Janno Kaprtisias <janno@occhio.nl>
2017-03-22 10:26:26 +0100

    added comments and fixed the package..json file

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ input.addEventListener('input', change);
 function change() {
   output.value = input.value;
 }
+//npm init -> zo begin je altijd een npm project.
+// je kan -y doen dan zeg je ja op alle vragen en kan
+// je deze aanpassen in je package.json file.
+//
